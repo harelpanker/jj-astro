200b7d2f0f8f59e14772649b563970d20ea608a7
Harel Panker <harelpanker@gmail.com>
2024-06-29 12:01:36 +0200

    first commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,19 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        alef: ['Alef, sans-serif', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        'dark-green': '#232E26',
+        'light-green': '#D9EFDE',
+        'theme-white': 'F2F4F8',
+      },
+    },
+  },
+  plugins: [],
+};
