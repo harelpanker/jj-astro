b0bf927f989603a025be4b4879c7f3c22abddd85
Harel Panker <harelpanker@gmail.com>
2024-07-01 08:53:08 +0200

    Adding seo stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import netlify from "@astrojs/netlify";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), sitemap()],
   output: "server",
   adapter: netlify()
 });
